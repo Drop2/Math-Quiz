3103d5ceb0127565330d8c5f56958a33bdc79824
Drop2 <95665308+Drop2@users.noreply.github.com>
2022-01-10 19:02:20 -0800

    Week 2 updates

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const prompt= require("prompt")
-
+/*
 prompt.start()
 
 prompt.get('color', function(err, result){
     if (err){
+        return onErr(err);
+    }
+    console.log("Did you say " + result.color);
+})
+*/
+const number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let first_number = number_list[Math.floor(Math.random()*number_list.length)]
+let second_number = number_list[Math.floor(Math.random()*number_list.length)]
+console.log(first_number, second_number)
+
+let answer = prompt.get('Answer', function(err, result){
+    if (err){
         return onerror(err);
     }
-    console.log("Did you say:" + result.color);
+    console.log(answer)
 })
